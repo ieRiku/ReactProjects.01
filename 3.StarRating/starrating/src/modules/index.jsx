db84e5ccb9ba2fec045ecdd9904f280ee5833bf1
echo <dummy@gmail.com>
2024-08-13 21:54:58 +0530

    add star-rating

---
+++ 3.StarRating/starrating/src/modules/index.jsx
@@ -0,0 +1,38 @@
+import { FaStar } from "react-icons/fa";
+import { useState } from 'react';
+import "./style.css";
+
+export default function StarRating({noOfStars = 5}) {
+    const[rating, setRating] = useState(0);
+    const[hover, setHover] = useState(0);
+    function handleClick(getCurrentIndex){
+        setRating(getCurrentIndex);
+        console.log(getCurrentIndex);
+    }
+    function handleMouseEnter(getCurrentIndex){
+        setHover(getCurrentIndex);
+        console.log(getCurrentIndex);
+    }
+    function handleMouseLeave(){
+        setHover(rating);
+        console.log("rating: "+rating);
+    }
+
+  return (
+    <div className="star-rating">
+      {[...Array(noOfStars)].map((_, index) => {
+        index += 1;
+        return (
+        <FaStar 
+            key={index}
+            className={index <= (hover || rating) ? 'acitve' : 'inactive'}
+            onClick={()=> handleClick(index)}
+            onMouseMove={()=> handleMouseEnter(index)}
+            onMouseLeave={()=> handleMouseLeave()}
+            size={40}
+        />
+        );
+      })}
+    </div>
+  );
+}
