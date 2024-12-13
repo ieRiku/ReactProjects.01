d2a1fe933f96901bad8884bad6107a8b9283f29d
Mriganka Patra <mriganka9432@gmail.com>
2024-12-13 23:39:33 +0530

    middle stage complete

--- 4.0.ImageSlider/image-slider/src/App.jsx
+++ 4.0.ImageSlider/image-slider/src/App.jsx
@@ -1,15 +1,69 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react";
+import {BsArrowLeftCircleFill, BsArrowRightCircleFill} from 'react-icons/bs';
 import './App.css'
 
 
-function App() {
+function App({url,limit=5, page=1}) {
   const [count, setCount] = useState(0)
+  const [images, setImages] = useState([])
+  const [currentSlide, setCurrentSlide] = useState(0)
+  const [errorMessage, setErrorMessage] = useState(null)
+  const [loading, setLoading] = useState(false)
+
+  async function fetchImages(getUrl) {
+    try{
+      setLoading(true)
+      const response = await fetch(`${getUrl}?page=${page}&limit=${limit}`);
+      const data = await response.json();
+      if(data){
+        setImages(data)
+        setLoading(false)
+      }
+    }
+    catch(e){
+      setErrorMessage(e.message);
+      setLoading(false);
+    }
+  }
+
+  useEffect(()=> {
+    if(url !== ""){
+      fetchImages(url)
+    }
+  },[url])
+
+  console.log(images);
+
+  if(loading){
+    return <div>Loading data! Please wait</div>;
+  }
+  if(errorMessage){
+    <div>Error occured! Please wait.</div>;
+  }
   
 
   return (
     <>
       <div className='container'>
-        I have written something.
+        <BsArrowLeftCircleFill className="arrow arrow-left"/>
+        {images && images.length ?
+          images.map(imageItem=>(
+            <img key={imageItem.id}
+            alt={imageItem.download_url}
+            src={imageItem.download_url}
+            className="current-image"
+            />
+          ))
+        : null}
+        <BsArrowRightCircleFill className="arrow arrow-right"/>
+        <span className="circle-indicators">
+          {
+            images && images.length ?
+              images.map((_, index)=> <button key={index} className="current-Indicator">
+
+              </button>)
+          : null}
+        </span>
       </div>
     </>
   )
--- 4.0.ImageSlider/image-slider/src/main.jsx
+++ 4.0.ImageSlider/image-slider/src/main.jsx
@@ -5,6 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <App url={'https://picsum.photos/v2/list'} page={'1'} limit={'10'}/>
   </StrictMode>,
 )
